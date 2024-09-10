6df9499e810fd4893b45f9b49183fb3ef9f2a454
spooketti <jonliu168@gmail.com>
2024-09-09 22:52:47 -0700

    audio recording system complete

--- static/js/record.js
+++ static/js/record.js
@@ -1,6 +1,9 @@
 let recorder
 let chunks = []
-
+let isRecording = false
+let recordButton = document.getElementById("recordButton")
+let audioPreview = document.getElementById("audioPreview")
+let audioFile = document.getElementById("audioFile")
 
 function AudioSetup()
 {
@@ -31,6 +34,41 @@ function StreamSetup(stream)
     {
         const blob = new Blob(chunks, {type: "audio/wav; codecs=opus"})
         chunks = []
-        
+        audioPreview.src = window.URL.createObjectURL(blob)
+        audioFile.src = window.URL.createObjectURL(blob)
     }
 }
+AudioSetup()
+
+function uploadAudio()
+{
+    audioFile.click()
+}
+
+function micToggle()
+{
+    if(isRecording)
+    {
+        isRecording = !isRecording
+        recorder.stop()
+        recordButton.classList.remove("isRecording")
+        return
+    }
+    recorder.start()
+    recordButton.classList.add("isRecording")
+    isRecording = !isRecording
+
+}
+
+audioFile.addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(event) {
+            const audio = event.target.result;
+            audioFile.src = audio
+            audioPreview.src = audio
+        };
+        reader.readAsDataURL(file);
+    }
+  });
